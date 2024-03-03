6a18c6d51234c492c4e9512f2fc1e20f72181529
MrMalikO <muhammadmalikomer@gmail.com>
2024-03-03 15:45:18 +0000

    Added a Node class, Created a no argument constructor. A set next method, a get next node method, and a getData method.

--- src/main/java/utils/LinkedList.java
+++ src/main/java/utils/LinkedList.java
@@ -10,8 +10,37 @@ import java.util.List;
  */
 public class LinkedList {
 
+    protected static class Node{
+        private String data;
+        private Node next;
+
+        public Node(String data){
+            this.data = data;
+            this.next = null;
+        }
+
+        public void setNext(Node next) {
+            this.next = next;
+        }
+
+        public Node getNext() {
+            return this.next;
+        }
+
+        public void setData(String data) {
+            this.data = data;
+        }
+
+        public String getData() {
+            return this.data;
+        }
+
+    }
+
+    private Node head;
+    private Node tail;
+    private int size;
 
 
 
-    
 }
