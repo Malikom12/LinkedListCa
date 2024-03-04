0dd20b01e360df86a67f958764129f275972e9d0
MrMalikO <muhammadmalikomer@gmail.com>
2024-03-04 23:32:53 +0000

    testAddToEmptyList() testAddDuplicateSong() testAddNonDuplicateSong() testAddMultipleSongsToEmptyList()

--- src/main/java/utils/LinkedList.java
+++ src/main/java/utils/LinkedList.java
@@ -14,6 +14,10 @@ public class LinkedList {
         private Song data;
         private Node next;
 
+        /**
+         * Constructs a new node with the given song data.
+         * @param data The song to be stored in the node.
+         */
         public Node(Song data){
             this.data = data;
             this.next = null;
@@ -41,6 +45,9 @@ public class LinkedList {
     private Node tail;
     private int songs;
 
+    /**
+     * Constructs an empty linked list.
+     */
     public LinkedList() {
         head = null;
         tail = null;
@@ -51,6 +58,12 @@ public class LinkedList {
         return songs;
     }
 
+    /**
+     * Gets the song at the specified position in the list.
+     * @param pos The position of the song to get.
+     * @return The song at the specified position.
+     * @throws IndexOutOfBoundsException If the position is out of range.
+     */
     public Song get(int pos) throws IndexOutOfBoundsException{
         if (pos < 0 || pos >= size()){
             throw new IndexOutOfBoundsException("The " + pos + " position entered is invalid! ");
@@ -65,10 +78,13 @@ public class LinkedList {
     }
 
 
-   /** A method called indexOf() that takes one parameter, a Song to be found in the list. The method should
-return an int indicating the position at which the Song could be found. If the Song could not be found, an
-    appropriate value should be returned to indicate this **/
 
+    /**
+     * Returns the index of the first occurrence of the specified song in the list,
+     * or -1 if the list does not contain the song.
+     * @param song The song to search for.
+     * @return The index of the specified song, or -1 if not found.
+     */
    public int indexOf(Song song){
        int i = 0;
        Node current = head;
@@ -84,18 +100,22 @@ return an int indicating the position at which the Song could be found. If the S
 
    /** A method called add() that takes a single parameter, a Song to be added to the end of the list. **/
 
+    /**
+     * Adds the specified song to the end of the list.
+     * @param song The song to be added.
+     */
    public void add(Song song) {
-       if (!isDuplicate(song)){
-           Node newNode = new Node(song);
-           if (head == null) {
-               head = newNode;
-           } else {
-               Node current = head;
-               while (current.getNext() != null) {
-                   current = current.getNext();
-               }
-               current.setNext(newNode);
+       Node newNode = new Node(song);
+       if (head == null) {
+           head = newNode;
+           songs++;
+       } else if (!isDuplicate(song)) {
+           Node current = head;
+           while (current.getNext() != null) {
+               current = current.getNext();
            }
+           current.setNext(newNode);
+           tail = current.getNext();
            songs++;
        }
    }
@@ -103,6 +123,10 @@ return an int indicating the position at which the Song could be found. If the S
    /** A method called isEmpty() that takes no parameters. This should return a boolean indicating if there is data
     currently stored in the list. **/
 
+    /**
+     * Checks if the list is empty.
+     * @return True if the list is empty, false otherwise.
+     */
    public boolean isEmpty(){
        if (head == null){
            return true;
@@ -114,6 +138,10 @@ return an int indicating the position at which the Song could be found. If the S
 
    /**A method called tail() that takes no parameters. This should return the last element in the list.**/
 
+    /**
+     * Returns the last element in the list.
+     * @return The last song in the list, or null if the list is empty.
+     */
    public Song tail(){
        if (head == null){
            return null;
@@ -123,20 +151,60 @@ return an int indicating the position at which the Song could be found. If the S
            while (current.getNext() != null) {
                current = current.getNext();
            }
+           tail = current;
            return current.getData();
        }
    }
 
-   public boolean isDuplicate(Song song){
-           Node current = head;
-           while(current!=null){
-               if (current.getData().equals(song)){
-                   return true;
-               }
-               current = current.getNext();
-           }
-       return false;
-   }
+    /**
+     * Checks if the specified song exists in the list.
+     * @param song The song to check for duplication.
+     * @return True if the song exists in the list, false otherwise.
+     */
+    public boolean isDuplicate(Song song){
+        Node current = head;
+        while(current != null){
+            if (current.getData().equals(song)){
+                return true;
+            }
+            current = current.getNext();
+        }
+        return false;
+    }
+
+
+    /**
+     * Removes all occurrences of the specified song from the list.
+     * @param song The song to be removed.
+     * @return True if the song was removed, false otherwise.
+     */
+    public boolean removeAll(Song song) {
+        if (song == null || isEmpty()) {
+            return false;
+        }
+
+        boolean flag = false;
+
+        while (head != null && head.getData().equals(song)) {
+            head = head.getNext();
+            flag = true;
+            songs--;
+        }
+
+        Node current = head;
+        while (current != null && current.getNext() != null) {
+            if (current.getNext().getData().equals(song)) {
+                current.setNext(current.getNext().getNext());
+                flag = true;
+                songs--;
+            } else {
+                current = current.getNext();
+            }
+        }
+
+        return flag;
+    }
+
 
 
 
--- src/test/java/utils/LinkedListExtraTests.java
+++ src/test/java/utils/LinkedListExtraTests.java
@@ -1,6 +1,9 @@
 package utils;
 
+import business.Song;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 /**
  *
@@ -8,6 +11,77 @@ import org.junit.jupiter.api.Test;
  */
 public class LinkedListExtraTests {
 
-    public LinkedListExtraTests() {
+    /**
+     * Test method for adding a song to an empty list.
+     * Verifies that the song is added correctly, list size increases, it's not empty,
+     * and the song becomes the tail of the list.
+     */
+    @Test
+    public void testAddToEmptyList() {
+        LinkedList list = new LinkedList();
+        Song song = new Song("the weeknd", "blinded by the light");
+        list.add(song);
+        assertEquals(1, list.size());
+        assertEquals(song, list.get(0));
+        assertFalse(list.isEmpty());
+        assertEquals(song, list.tail());
     }
+
+    /**
+     * Test method for adding a duplicate song to the list.
+     * Verifies that only one instance of the duplicate song is added to the list.
+     */
+    @Test
+    public void testAddDuplicateSong() {
+        LinkedList list = new LinkedList();
+        Song song = new Song("shaun mendes", "treat you better");
+        list.add(song);
+        list.add(song);
+        assertEquals(1, list.size());
+    }
+
+    /**
+     * Test method for adding non-duplicate songs to the list.
+     * Verifies that each song is added correctly, list size increases,
+     * songs are retrievable, and the last added song becomes the tail.
+     */
+    @Test
+    public void testAddNonDuplicateSong() {
+        LinkedList list = new LinkedList();
+        Song song1 = new Song("charlie puth", "attention");
+        Song song2 = new Song("justing beibs", "company");
+        list.add(song1);
+        list.add(song2);
+        assertEquals(2, list.size());
+        assertEquals(song1, list.get(0));
+        assertEquals(song2, list.get(1));
+        assertFalse(list.isEmpty());
+        assertEquals(song2, list.tail());
+    }
+
+    /**
+     * Test method for adding multiple songs to an initially empty list.
+     * Verifies that all songs are added correctly, list size matches the number of added songs,
+     * songs are retrievable, and the last added song becomes the tail.
+     */
+    @Test
+    public void testAddMultipleSongsToEmptyList() {
+        LinkedList list = new LinkedList();
+        Song song1 = new Song("selina", "wolves");
+        Song song2 = new Song("ariana", "grande");
+        Song song3 = new Song("camila", "cabello");
+        list.add(song1);
+        list.add(song2);
+        list.add(song3);
+        assertEquals(3, list.size());
+        assertEquals(song1, list.get(0));
+        assertEquals(song2, list.get(1));
+        assertEquals(song3, list.get(2));
+        assertFalse(list.isEmpty());
+        assertEquals(song3, list.tail());
+    }
+
+
+
+
 }
